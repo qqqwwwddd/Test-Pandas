3254ac51fd1c3a9251656fd87cf4b9ab3efbe2b0
qqqwwwddd <qqqwwwddd1@gmail.com>
2021-10-26 13:58:51 +0900

    fish_length, fish_weight 만들기

--- fish_api.py
+++ fish_api.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlalchemy as db
 import matplotlib.pyplot as plt
 
+#1. 불러오기
 bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
 #print(bream_length)
 bream_weight= pd.read_csv("bream_weight.csv").to_numpy().flatten()
@@ -12,6 +13,15 @@ smelt_length = pd.read_csv("smelt_length.csv").to_numpy().flatten()
 smelt_weight = pd.read_csv("smelt_weight.csv").to_numpy().flatten()
 #print(smelt_weight)
 
+#2. bream_data, smelt_data 시각화
 plt.scatter(bream_length,bream_weight)
 plt.scatter(smelt_length,smelt_weight)
-plt.show()
+# plt.show()
+
+#3. fish_length, fish_weight 만들기
+fish_length = np.concatenate((bream_length, smelt_length))
+# print(fish_length.shape)
+fish_weight = np.concatenate((bream_weight, smelt_weight))
+# print(fish_weight.shape)
+fish_data = np.column_stack((fish_length, fish_weight)) 
+print(fish_data)
