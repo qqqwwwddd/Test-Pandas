512ed2692513475cc32fb0994e5945bd7c206a45
qqqwwwddd <qqqwwwddd1@gmail.com>
2021-10-26 12:44:45 +0900

    1. csv 불러오기, 컬럼명 적기

---
+++ fish_api.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import sqlalchemy as db
+import matplotlib.pyplot as plt
+
+bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
+#print(bream_length)
+bream_weight= pd.read_csv("bream_weight.csv").to_numpy().flatten()
+#print(bream_weight)
+smelt_length = pd.read_csv("smelt_length.csv").to_numpy().flatten()
+#print(smelt_length)
+smelt_weight = pd.read_csv("smelt_weight.csv").to_numpy().flatten()
+#print(smelt_weight)
